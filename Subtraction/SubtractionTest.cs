7045b8f56c64402d8403a57259c1881f41672963
Clara230 <58736582+Clara230@users.noreply.github.com>
2020-01-08 14:29:56 +0200

    Subtraction

--- Subtraction/SubtractionTest.cs
+++ Subtraction/SubtractionTest.cs
@@ -3,8 +3,28 @@
 namespace Subtraction
 {
     public class SubtractionTest
+{
+    [TestClass()]
+    public class SubtractionTests
     {
-        public
+        private readonly int a = 15;
+        private readonly int b = 5;
+        // private readonly double c = 2.5;
+        // private readonly double d = 5.5;
+        [TestMethod()]
+        public void SubtractTest()
+        {
+            Assert.AreEqual(10, Subtraction.Subtract(a, b));
+
+        }
+
+        [TestMethod()]
+        public void SubtractTest1()
+        {
+            Assert.AreEqual(10, Subtraction.Subtract(a, b));
+        }
+    }
+}
 
     }
 }
