90cd6a77902059ff62c7a4cc5a5f8b3969446385
Clara230 <58736582+Clara230@users.noreply.github.com>
2020-01-08 14:34:52 +0200

    Division

--- DivisionTest/Division.cs
+++ DivisionTest/Division.cs
@@ -4,5 +4,23 @@ namespace DivisionTest
 {
     public class Division
     {
+        private readonly int a = 10;
+        private readonly int b = 2;
+        private readonly double c = 50.5;
+        private readonly double d = 2.5;
+        [TestMethod()]
+        public void DivideTest()
+        {
+            Assert.AreEqual(5, Division.Divide(a, b));
+        }
+
+        [TestMethod()]
+        public void DivideDoubleTest()
+        {
+            Assert.AreEqual(20.2, Division.Divide(c, d));
+        }
+    }
+}
+
     }
 }
